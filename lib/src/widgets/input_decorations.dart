c4962243bc33365375de95c36992a366b0a30973
Gabi <dugabrielle9@gmail.com>
2025-05-01 15:49:26 -0300

    redução do fontsize

--- lib/src/widgets/input_decorations.dart
+++ lib/src/widgets/input_decorations.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class InputStyleDecoration {
   static InputDecoration style() {
     return const InputDecoration(
-      errorStyle: TextStyle(fontSize: 18),
+      errorStyle: TextStyle(fontSize: 16),
       errorMaxLines: 3,
     );
   }
